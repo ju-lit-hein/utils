ae9ed67085eaa5042e206bb91ce240d08266467c
ju-lit-hein <julien.ferdinand@epitech.eu>
2023-08-01 19:55:08 +0400

    bashrc modifications

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+command gk epitech
 # do not config here
 # go to ./conf.d/ and create and appropriate [category].fish file
